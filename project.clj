4695950380448b98851d79458611a217804eb2c8
Rafal Dittwald <rafal.dittwald@gmail.com>
2025-10-24 14:43:38 -0400

    Bump omni to fix sci issue in prod

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject clojurecamp.currmap "0.1.0-SNAPSHOT"
 
-  :dependencies [[io.bloomventures/omni "0.32.2"]
+  :dependencies [[io.bloomventures/omni "0.34.1"]
                  [io.bloomventures/commons "0.15.1"]
                  ;; require sci to fix edamame deps issue
-                 [borkdude/sci "0.2.7"]
                  [datascript "1.4.2"]
                  [tada "0.2.2"]
                  [metosin/malli "0.19.1"]
@@ -12,7 +11,7 @@
                  [commons-validator/commons-validator "1.10.0"]
                  [com.hyperfiddle/rcf "20220926-202227"]]
 
-  :plugins [[io.bloomventures/omni "0.32.2"]]
+  :plugins [[io.bloomventures/omni "0.34.1"]]
 
   :omni-config clojurecamp.currmap.omni-config/omni-config
 
