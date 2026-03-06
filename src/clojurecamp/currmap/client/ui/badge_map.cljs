4c028433eadc0f67513e59677100e3fc451f7a66
Rafal Dittwald <rafal.dittwald@gmail.com>
2026-03-06 17:57:40 -0500

    Visually indicate which badges a user has

--- src/clojurecamp/currmap/client/ui/badge_map.cljs
+++ src/clojurecamp/currmap/client/ui/badge_map.cljs
@@ -3,6 +3,7 @@
    [bloom.commons.pages :as pages]
    [reagent.core :as r]
    [clojurecamp.currmap.client.ui.bezier :as bezier]
+   [clojurecamp.currmap.client.state :as state]
    [goog.object :as o]))
 
 (defn wait-for [get-value]
@@ -49,51 +50,66 @@
                  (reset! *layout l)))
         (.catch js/console.error))))
 
+(defn pure-layout-view
+  [{:keys [layout badges-by-id earned-badge-ids]}]
+  [:svg {:style {:width (.-width layout)
+                 :height (.-height layout)}}
+   (for [{:strs [id badge-id width height x y]}
+         (js->clj (.-children layout))
+         :let [badge (badges-by-id badge-id)
+               earned? (contains? earned-badge-ids badge-id)]]
+     ^{:key id}
+     [:g {:tw "cursor-pointer"
+          :on-click
+          (fn []
+            (pages/navigate-to! [:badge {:badge-id badge-id}]))}
+      [:rect {:width width
+              :height height
+              :x x
+              :y y
+              :fill (if earned? "#2563eb" "lightgray")}]
+      [:text {:x (+ x (/ width 2))
+              :y (+ y (/ height 2))
+              :text-anchor "middle"
+              :alignment-baseline "middle"
+              :fill (if earned? "white" "black")}
+       (:badge/name badge)]])
+
+   (for [{:strs [id sections]}
+         (js->clj (.-edges layout))]
+     ^{:key id}
+     [:g
+      (for [{:strs [id] :as section} sections]
+        ^{:key id}
+        [:path {:d (bezier/get-bezier-path-from-points section)
+                :stroke "black"
+                :stroke-width 1
+                :fill "none"}]
+        #_[:line {:x1 (get startPoint "x")
+                  :y1 (get startPoint "y")
+                  :x2 (get endPoint "x")
+                  :y2 (get endPoint "y")
+                  :style {:stroke "black"
+                          :strokeWidth 2}}])])])
+
 (defn layout-view [badges]
   (r/with-let
-   [*layout (r/atom nil)
-    _ (-> (wait-for (fn []
-                      (o/get js/window "ELK")))
-          (.then (fn []
-                   (layout! *layout badges))))]
-   [:div {:tw "relative w-80vw h-90vh"}
-    (when @*layout
-      [:svg {:style {:width (.-width @*layout)
-                     :height (.-height @*layout)}}
-       (let [badges-by-id (zipmap (map :badge/id badges)
-                                  badges)]
-         (for [{:strs [id badge-id width height x y]}
-               (js->clj (.-children @*layout))
-               :let [badge (badges-by-id badge-id)]]
-           ^{:key id}
-           [:g {:tw "cursor-pointer"
-                :on-click
-                (fn []
-                  (pages/navigate-to! [:badge {:badge-id badge-id}]))}
-            [:rect {:width width
-                    :height height
-                    :x x
-                    :y y
-                    :fill "lightgray"}]
-            [:text {:x (+ x (/ width 2))
-                    :y (+ y (/ height 2))
-                    :text-anchor "middle"
-                    :alignment-baseline "middle"}
-             (:badge/name badge)]]))
-
-       (for [{:strs [id sections]}
-             (js->clj (.-edges @*layout))]
-         ^{:key id}
-         [:g
-          (for [{:strs [id] :as section} sections]
-            ^{:key id}
-            [:path {:d (bezier/get-bezier-path-from-points section)
-                    :stroke "black"
-                    :stroke-width 1
-                    :fill "none"}]
-            #_[:line {:x1 (get startPoint "x")
-                      :y1 (get startPoint "y")
-                      :x2 (get endPoint "x")
-                      :y2 (get endPoint "y")
-                      :style {:stroke "black"
-                              :strokeWidth 2}}])])])]))
+    [*layout (r/atom nil)
+     _ (-> (wait-for (fn []
+                       (o/get js/window "ELK")))
+           (.then (fn []
+                    (layout! *layout badges))))]
+    [:div {:tw "relative w-80vw h-90vh"}
+     (when @*layout
+       [pure-layout-view
+        {:layout @*layout
+         :badges-by-id (zipmap (map :badge/id badges)
+                               badges)
+         :earned-badge-ids (set @(state/q '[:find [?badge-id ...]
+                                            :in $ ?user-id
+                                            :where
+                                            [?u :user/id ?user-id]
+                                            [?a :assertion/user ?u]
+                                            [?a :assertion/badge ?b]
+                                            [?b :badge/id ?badge-id]]
+                                          (:user/id @state/user)))}])]))
